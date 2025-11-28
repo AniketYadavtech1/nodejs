86355491bf796dd502be7536e4660086df04d9ab
Aniket Yadav <aniketyadavtech1@gmail.com>
2025-11-28 23:46:19 +0530

    ak

--- js/file.js
+++ js/file.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 
 
 // Sync..... file 
+// create file 
 
 // fs.writeFileSync("./test.txt","Hey There");
-
+// a syncrosh
 fs.writeFile("./test.txt","Hey There", "Hello World",(err)=>{})
